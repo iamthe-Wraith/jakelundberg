7067a14809365dd6561b1b2050a783ea036e63a0
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-07-07 14:20:59 -0400

    feat: adds currently reading list to homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,11 +2,13 @@ import type { PageServerLoad } from "./$types";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import { BlogService, type IBlogPost } from "$lib/services/blog";
+import { LibraryService, type IBook } from "$lib/services/library";
 
 dayjs.extend(utc);
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
     let posts: IBlogPost[] = [];
+    let currentlyReading: IBook[] = [];
 
     try {
         const blogService = new BlogService();
@@ -15,9 +17,17 @@ export const load: PageServerLoad = async () => {
         // swallowing error
     }
 
+    try {
+        const libraryService = new LibraryService(locals.sanity);
+        currentlyReading = await libraryService.getCurrentlyReading();
+    } catch {
+        // swallowing
+    }
+
     return {
         blog: {
             posts,
-        }
+        },
+        currentlyReading,
     };
 };
