17505dfc5b9565b7e42d6297fbd588b0ea2fb57d
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-07-26 07:40:10 -0400

    feat: adds new EnvironmentImg component for main img on page

---
+++ src/lib/components/EnvironmentImg.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string;
+</script>
+
+<div class="environment-img">
+    <img src="{src}" alt="{alt}" />
+</div>
+
+<style>
+    .environment-img {
+        width: 100%;
+        max-width: 80rem;
+        margin: 0 auto;
+        overflow: hidden;
+
+        & img {
+            width: 100%;
+            height: auto;
+            object-fit: cover;
+        }
+    }
+</style>
