4f9c4ab88a01b439ede8c41c4b0c9a17d67c47d1
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-11-07 13:17:54 -0500

    refactor: updates design and verbiage of experience page

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+    type CheckboxType = 'primary' | 'accent1' | 'accent2' | 'accent3' | 'neutral' | 'danger';
+
+    export let id: string;
+    export let name: string;
+    export let checked = false;
+    export let type: CheckboxType = 'primary';
+</script>
+
+<div class="container">
+    <label for={id} data-testid={id}>
+        <input
+            {...$$restProps}
+            {id}
+            {name}
+            type="checkbox"
+            aria-labelledby="{id}-label"
+            data-testid="{id}-input"
+            bind:checked={checked}
+        />
+        
+        <div
+            class="checkbox {type} {checked ? 'checked' : ''}"
+            data-testid="{id}-checkbox"
+        >
+            <i
+                class="fa-regular fa-check"
+                data-testid="{id}-checkbox-icon"
+            >
+            </i>
+        </div>
+    
+        <div id="{id}-label" data-testid="{id}-label">
+            <slot></slot>
+        </div>
+    </label>
+</div>
+
+<style>
+    .container {
+        padding: var(--outline-offset);
+    }
+
+    label {
+        position: relative;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        padding-right: 0.25rem;
+        border-radius: 0.25rem;
+        overflow: hidden;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    input {
+        position: absolute;
+        top: -9px;
+        left: -9px;
+        width: 0;
+        height: 0;
+        opacity: 0;
+
+        &:checked {
+            & + .checkbox i {
+                display: block;
+            }
+        }
+    }
+
+    .checkbox {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 1.75rem;
+        height: 1.75rem;
+        border-radius: 0.25rem;
+        
+        &.primary {
+            border: 1px solid var(--primary-500);
+        }
+
+        &.accent1 {
+            border: 1px solid var(--accent1-500);
+        }
+
+        &.accent2 {
+            border: 1px solid var(--accent2-500);
+        }
+
+        &.accent3 {
+            border: 1px solid var(--accent3-500);
+        }
+
+        &.neutral {
+            border: 1px solid var(--neutral-350);
+        }
+
+        &.danger {
+            border: 1px solid var(--danger-500);
+        }
+
+        & i {
+            display: none;
+            font-size: 1.25rem;
+        }
+    }
+</style>
--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -6,10 +6,11 @@
 	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 	import { assets } from '$lib/stores/assets';
 	import { assets as assetsPath } from '$app/paths';
-	import Bugz from "$lib/components/Bugz.svelte";
 	import TextBlock from "$lib/components/TextBlock.svelte";
 	import Intro from "$lib/components/Intro.svelte";
 	import Section from "$lib/components/Section.svelte";
+	import IntroContent from "$lib/components/IntroContent.svelte";
+	import Checkbox from "$lib/components/Checkbox.svelte";
 
 	dayjs.extend(utc);
 
@@ -36,64 +37,59 @@
 			);
 		});
 	}
-</script>
 
-<!-- <EnvironmentImg
-	src="https://res.cloudinary.com/dxpwpno1e/image/upload/c_scale,w_1024/v1720401233/cemetery_sgsaei.png"
-	alt="A spooky cemetery in the mountains at night with crooked tombstones and creepy trees."
-/> -->
+	const onReadMoreToggle = (projectId: number) => {
+		return () => {
+			const container: HTMLDivElement | null = document.querySelector(`#project-${projectId} .description-container`);
+
+			if (container) {
+				const desc: HTMLDivElement | null = container.querySelector('.description');
+				if (desc) {
+					const computedStyle = window.getComputedStyle(desc);
+					container.style.height = !container.style.height || container.style.height === '0px' ? computedStyle.height : '0';
+				}
+			}
+		}
+	}
+</script>
 
 <div class="container">
 	<Intro header="Experience">
-		<div class="intro-content row">
-			<div>
-				<p>
-					Jake has been building stuff for the web for a long time. But even after more than a decade of experience, he
-					still gets excited about learning new things and building new stuff. I've never seen anyone so passionate about
-					what they do!
-				</p>
+		<IntroContent>
+			<p>
+				I've been building stuff for the web for a long time. But even after all that time, I still get excited about
+				learning new things and building new stuff.
+			</p>
 
-				<p>
-					Not only does he like learning new things, but he also loves using what he knows to help solve problems. He's
-					always looking for ways to make things better. Wether it's optimizing an application's performance, automating
-					some tedious task, or just making something easier to use, he's constantly working to make the web, and the world
-					around him a better, more enjoyable place.
-				</p>
-			</div>
-	
-			<div class="img-container">
-				<Bugz is="waving" />
-			</div>
-		</div>
+			<p>
+				Not only do I like learning new things, but I also loves using what I knows to help solve problems. I'm
+				always looking for ways to make things better. Wether it's optimizing an application's performance, automating
+				some tedious task, or just making something easier to use, I'm constantly working to make the web, and the world
+				around me a better, more enjoyable place.
+			</p>
+		</IntroContent>
 	</Intro>
 
 	<Section>
 		<div class="work">
 			<h2>Work Stuff</h2>
 	
-			<div class="row">
-				<div class="img-container">
-					<Bugz is="waving" />
-				</div>
-	
-				<div class="section-intro">
-					{#if data.jobs.error}
-						<p class="error">
-							{data.jobs.error}
-						</p>
-					{:else}
-						<p>
-							Jake has had the opportunity to work at some really cool places over the years. From client management
-							tools, to environmental initiatives, to financial services, he's had the chance to work on a wide variety
-							of projects and with some amazing people. And everywhere he goes he brings his passion for building
-							software and making things better.
-						</p>
+			<div class="section-intro">
+				{#if data.jobs.error}
+					<p class="error">
+						{data.jobs.error}
+					</p>
+				{:else}
+					<p>
+						I've had the privilege of working on some really cool projects over the years. From client management
+						tools, to financial services, to environmental initiatives, I've gotten to work at a wide variety
+						of businesses with some amazing people.
+					</p>
 
-						<p>
-							Here are a few of the places he's worked and some of the things he's done. 
-						</p>
-					{/if}
-				</div>
+					<p>
+						Here are a few of the places I've worked and some of the things I've done. 
+					</p>
+				{/if}
 			</div>
 	
 			{#if data.jobs.data.length && !data.jobs.error}
@@ -103,7 +99,7 @@
 							<a
 								href="{job.url}" 
 								target="_blank" 
-								class="company-logo-container"
+								class="img-container company-logo-container"
 							>
 								<img
 									class="company-logo"
@@ -114,13 +110,15 @@
 	
 							<div class="content">
 								<div class="header">
-									<a href="{job.url}" target="_blank" class="h5 title">
+									<h3 class="h5 title">
 										{job.position}&#12539;{job.companyName}
-									</a>
+									</h3>
 								</div>
 		
 								<div class="metadata">
-									<p>{job.location}</p>
+									<p>
+										{dayjs(job.startDate).local().format('MMM, YYYY')} - {job.endDate ? dayjs(job.endDate).local().format('MMM, YYYY') : 'Present'}
+									</p>
 								</div>
 				
 								{#if job.summary}
@@ -152,46 +150,65 @@
 		<div class="projects">
 			<h2>Projects</h2>
 	
-			<div class="row">
-				<div class="img-container">
-					<Bugz is="waving" />
-				</div>
-	
-				<div class="section-intro">
-					{#if data.projects.error}
-						<p class="error">
-							{data.projects.error}
-						</p>
-					{:else}
-						<p>
-							Jake is also one of those developers that just loves to build things. So he's always got some project or another
-							that he's working on. He tells me about them when it's my bedtime, and they always sound so exciting!
-						</p>
+			<div class="section-intro">
+				{#if data.projects.error}
+					<p class="error">
+						{data.projects.error}
+					</p>
+				{:else}
+					<p>
+						I honestly love what I do. Building software and solving problems with technology just feels like
+						something I was born to do. That's why, even when I'm not at work, you'll often find me
+						hacking away on some personal project.
+					</p>
 
-						<p>
-							Here are a few of the projects he's built over the years. I hope you find them as interesting as I do!
-						</p>
-					{/if}
-				</div>
+					<p>
+						Here are a few of the projects I've built over the years.
+					</p>
+				{/if}
 			</div>
 	
 			{#if data.projects.data.length && !data.projects.error}
 				<div class="list-container">
 					{#each data.projects.data as project, i (project.id)}
-						<article class="{(i % 2) ? 'reverse' : ''}">
-							<a href="{project.url}" target="_blank" class="project-img-container">
+						<article class="{(i % 2) ? 'reverse' : ''}" id="project-{project.id}">
+							<div class="img-container project-img-container">
 								<img
 									class="project-img"
 									src="{project.image}"
 									alt="project preview"
 								/>
-							</a>
+							</div>
 	
 							<div class="content">
 								<div class="header">
-									<a href="{project.url}" target="_blank" class="h5 title">
+									<h3 class="h5 title">
 										{project.name}
-									</a>
+									</h3>
+
+									{#if project.url || project.links?.length}
+										<div class="links">
+											{#if project.url && !(project.links ?? []).find(link => link.url === project.url)}
+												<a
+													href="{project.url}"
+													target="_blank"
+													class="link"
+												>
+													View Project
+												</a>
+											{/if}
+
+											{#each (project.links ?? []) as link}
+												<a
+													href="{link.url}"
+													target="_blank"
+													class="link"
+												>
+													{link.label}
+												</a>
+											{/each}
+										</div>
+									{/if}
 								</div>
 				
 								{#if project.summary}
@@ -201,22 +218,24 @@
 								{/if}
 
 								{#if project.description}
-									<div class="description">
-										<TextBlock text={project.description} />
-									</div>
-								{/if}
-	
-								{#if project.links?.length}
-									<div class="links">
-										{#each project.links as link}
-											<a
-												href="{link.url}"
-												target="_blank"
-												class="link"
-											>
-												{link.label}
-											</a>
-										{/each}
+									<div>
+										<div class="description-container">
+											<div class="description">
+												<TextBlock text={project.description} />
+											</div>
+										</div>
+
+										<div class="read-more-toggle">
+											<input
+												type="checkbox"
+												id="description-toggle-{project.id}"
+												on:change={onReadMoreToggle(project.id)}
+											/>
+											<label for="description-toggle-{project.id}">
+												<span class="read-more">Read More</span>
+												<span class="read-less">Read Less</span>
+											</label>
+										</div>
 									</div>
 								{/if}
 	
@@ -246,23 +265,8 @@
 		margin: 0 auto;
 	}
 
-	.row {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-
-		& .img-container {
-			display: flex;
-			justify-content: center;
-		}
-
-		@media (min-width: 500px) {
-			flex-direction: row;
-			gap: 0.5rem;
-		}
-	}
-
 	.section-intro {
+		margin-bottom: 2rem;
 		padding: 0 0.5rem;
 
 		&.error {
@@ -286,183 +290,213 @@
 		border: 5px solid var(--neutral-300);
 		transition: 0.2s ease-in-out;
 
-		&:has(a:not(.tech):hover),
-		&:has(a:not(.tech):focus-visible) {
+		&:has(a:not(.tech):not(.link):hover) {
 			border: 5px solid var(--neutral-400);
-			background: var(--neutral-200);
-			transform: scale(1.02);
+			
+			& img {
+				transform: scale(1.05) rotateZ(-6deg);
+			}
 		}
 
 		&:not(:last-of-type) {
 			margin-bottom: 3rem;
 		}
+	}
 
-		& .company-logo-container,
-		& .project-img-container {
-			display: flex;
-			justify-content: center;
-			margin: 0 auto 1rem;
-			padding: 1rem;
-		}
+	.img-container {
+		display: flex;
+		justify-content: center;
+		margin: 0 auto 1rem;
+		padding: 1rem;
 
-		& .company-logo,
-		& .project-img {
+		& img {
 			display: block;
 			width: 100%;
 			max-width: 20rem;
 			height: auto;
 			object-fit: contain;
 			object-position: center;
+			transition: 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
 		}
+	}
 
-		& .dates,
-		& .metadata {
-			& p,
-			& p * {
-				margin: 0;
-				font-size: 0.75rem;
-				color: var(--neutral-500);
+	.company-logo-container {
+		align-items: center;
+	}
+
+	.project-img-container {
+		align-items: flex-start;
+	}
+
+	.metadata {
+		margin-bottom: 0.5rem;
+		text-align: center;
+
+		& p,
+		& p * {
+			margin: 0;
+			font-size: 0.75rem;
+			color: var(--neutral-500);
+		}
+	}
+
+	.links {
+		display: flex;
+		justify-content: center;
+		margin-bottom: 1rem;
+
+		& a {
+			padding: 0 0.5rem;
+			font-size: 0.875rem;
+			color: var(--accent1-500);
+			text-decoration: none;
+			line-height: 1.2rem;
+
+			&:not(:last-of-type) {
+				border-right: 1px solid var(--neutral-300);
+			}
+
+			&:hover {
+				color: var(--accent1-600);
+			}
+		}
+	}
+
+	.title {
+		display: block;
+		margin: 0;
+		text-align: center;
+		text-decoration: none;
+	}
+
+	.description-container {
+		height: 0;
+		transition: height 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+		overflow: hidden;
+	}
+	
+	.description {
+		padding: 1rem 0;
+	}
+
+	.read-more-toggle {
+		display: flex;
+		justify-content: center;
+		gap: 0.5rem;
+		padding: 0.5rem 0;
+
+		&:hover {
+			cursor: pointer;
+
+			& .read-more,
+			& .read-less {
+				color: var(--neutral-600);
+				cursor: pointer;
 			}
 		}
 
-		& .links {
-			display: flex;
-			justify-content: center;
-			gap: 1rem;
-
-			& a {
-				color: var(--accent1-500);
-			}
-		}
-
-		& .dates {
-			position: relative;
-
-			& p {
-				position: absolute;
-				top: 30%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-			}
-		}
-		
-		& .tombstone {
-			position: absolute;
-			bottom: 0;
-			left: 50%;
-			width: 20rem;
-			margin: -3rem auto 0;
-			aspect-ratio: 1 / 1;
-			transform: translateX(-50%);
-		}
-
-		& .metadata {
-			margin-bottom: 0.5rem;
-			text-align: center;
-		}
-
-		& .content {
-			& .title {
+		&:has(input:checked) {
+			& .read-less {
 				display: block;
-				margin: 0;
-				text-align: center;
-				text-decoration: none;
 			}
 
-			& .techs {
-				display: flex;
-				justify-content: flex-end;
-				gap: 0.25rem;
-				flex-wrap: wrap;
-				margin-top: 1rem;
-
-				& .tech {
-					margin-bottom: 0.25rem;
-					padding: 0 0.5rem;
-					font-size: 0.85rem;
-					background: var(--neutral-900);
-					color: var(--neutral-100);
-					font-size: 0.8rem;
-					line-height: 1.3rem;
-					text-decoration: none;
-
-					&:hover,
-					&:focus-visible {
-						background-color: var(--accent2-500);
-						color: var(--neutral-900);
-					}
-				}
-
-				& .tech:nth-child(1n) {
-					transform: rotate(-3deg);
-				}
-
-				& .tech:nth-child(2n) {
-					transform: rotate(-1deg);
-				}
-
-				& .tech:nth-child(3n) {
-					transform: rotate(2deg);
-				}
+			& .read-more {
+				display: none;
 			}
 		}
 
-		@media (min-width: 768px) {
+		& input {
+			width: 0;
+			height: 0;
+			opacity: 0;
+		}
+
+		& input:focus-visible ~ label {
+			outline: 1px dashed var(--neutral-800);
+			outline-offset: calc(var(--outline-offset) - 1px);
+		}
+
+		& .read-more,
+		& .read-less {
+			color: var(--neutral-500);
+		}
+
+		& .read-less {
+			display: none;
+		}
+
+		& .read-more {
+			display: block;
+		}
+	}
+
+	.techs {
+		display: flex;
+		justify-content: flex-end;
+		gap: 0.25rem;
+		flex-wrap: wrap;
+		margin-top: 1rem;
+
+		& .tech {
+			margin-bottom: 0.25rem;
+			padding: 0 0.5rem;
+			font-size: 0.85rem;
+			background: var(--neutral-900);
+			color: var(--neutral-100);
+			font-size: 0.8rem;
+			line-height: 1.3rem;
+			text-decoration: none;
+
+			&:hover,
+			&:focus-visible {
+				background-color: var(--accent2-500);
+				color: var(--neutral-900);
+			}
+		}
+
+		& .tech:nth-child(1n) {
+			transform: rotate(-3deg);
+		}
+
+		& .tech:nth-child(2n) {
+			transform: rotate(-1deg);
+		}
+
+		& .tech:nth-child(3n) {
+			transform: rotate(2deg);
+		}
+	}
+
+	@media (min-width: 768px) {
+		article {
 			flex-direction: row;
 			align-items: stretch;
 			gap: 1rem;
 			padding: 0.5rem 1rem;
-			
-			&.reverse {
-				flex-direction: row-reverse;
+		}
 
-				& .content .tags {
-					justify-content: flex-start;
-				}
-			}
+		article:not(:last-of-type) {
+			margin-bottom: 4rem;
+		}
+		
+		article.reverse {
+			flex-direction: row-reverse;
+		}
+		.img-container img {
+			min-width: 15rem;
+			max-width: 15rem;
+		}
 
-			&:not(:last-of-type) {
-				margin-bottom: 4rem;
-			}
+		.company-logo-container {
+			margin: 0;
+		}
 
-			& .company-logo-container {
-				margin: 0;
-			}
+		.content {
+			flex-grow: 1;
+		}
 
-			& .project-img,
-			& .company-logo {
-				min-width: 15rem;
-				max-width: 15rem;
-			}
-
-			& .dates {
-				display: flex;
-				justify-content: flex-end;
-				max-width: 15rem;
-				min-width: 15rem;
-				padding-top: 0.3rem;
-
-				& p {
-					text-align: center;
-				}
-			}
-
-			& .tombstone {
-				top: 50%;
-				left: unset;
-				right: 0;
-				bottom: unset;
-				margin: 0;
-				transform: translate(0, -50%);
-			}
-
-			& .content {
-				flex-grow: 1;
-
-				& .techs {
-					margin: 1rem 0 -1.5rem 0;
-				}
-			}
+		.techs {
+			margin: 1rem 0 -1.5rem 0;
 		}
 	}
 </style>
