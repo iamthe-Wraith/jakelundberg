f6ba9333fca58ca1a8899e6bb7b78d818e37ac74
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-11-08 11:34:11 -0500

    refactor: updates uses page

--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-	import Bugz from "$lib/components/Bugz.svelte";
 	import Intro from "$lib/components/Intro.svelte";
 	import Section from "$lib/components/Section.svelte";
-
 </script>
 
 <div class="container">
@@ -10,19 +8,14 @@
 		<div class="intro-content row">
 			<div>
 				<p>
-					This is a list of the hardware and software Jake is currently using on a daily basis. He's always on the lookout for
-					tool that will help him be more productive and efficient, so if you have any suggestions, please let us know!
-
+					This is a list of the hardware and software I'm currently using on a daily basis. I'm always on the lookout for
+					tools that will help me be more productive and efficient, so if you have any suggestions, please let me know!
 				</p>
 
 				<p>
 					Oh, and just a heads up, most of these are Amazon affiliate links, so be sure to click them and buy something
-					so Jake can keep the lights on around here!
+					so I can keep the lights on around here! 😆
 				</p>
-			</div>
-	
-			<div class="img-container">
-				<Bugz is="waving" />
 			</div>
 		</div>
 	</Intro>
@@ -32,78 +25,78 @@
 
 		<ul>
 			<li>
-				Jake recently updated his desk to a <a href="https://amzn.to/47abisC" target="_blank">HUANUO 48″ x 24″ Electric Standing Desk</a>
-				and has been really happy with it. It's not as fancy as some of the other desks out there, but it's sturdy, has a good range of motion, and the price was right.
+				I recently updated my desk to a <a href="https://amzn.to/47abisC" target="_blank">HUANUO 48″ x 24″ Electric Standing Desk</a>
+				and I've been really happy with it. It's not as fancy as some of the other desks out there, but it's sturdy, has a good range of motion, and the price was right.
 			</li>
 			<li>
-				His desk chair is a <a href="https://secretlabchairs.ca/products/titan-evo-2022-series?sku=R22PU-Batman" target="_blank">Secretlab Titan Evo</a> emblazened
+				My desk chair is a <a href="https://secretlabchairs.ca/products/titan-evo-2022-series?sku=R22PU-Batman" target="_blank">Secretlab Titan Evo</a> emblazened
 				with the Batman logo with <a href="https://amzn.to/3Td6ZXE" target="_blank">clear roller blade wheels</a> that are super smooth, even on carpet.
 			</li>
 			<li>
-				He is currently using a 2021 16" Macbook Pro M1 Max with 64 GB ram and a 2TB SSH. And of course, it's covered in stickers!
+				I'm currently using a 2021 16" Macbook Pro M1 Max with 64 GB ram and a 2TB SSH. And of course, it's covered in stickers!
 			</li>
 			<li>
-				Jake's main monitor is a <a href="https://www.sceptre.com/Monitors/2K-4K-Series/C355W-3440UN-35-Curved-Monitor-product1176category12category98.html" target="_blank">Scepter C355W-3440UN 35" Curved Monitor</a>.
-				And above it are two <a href="https://www.acer.com/gb-en/monitors/essential/s1/pdp/UM.HS1EE.C01" target="_blank">27" Acer S271HL Widescreen LCD Monitors</a> on separate <a href="https://amzn.to/3Xcm2Ca" target="_blank">VIVO Triple LCD LED Computer Monitor Desk Stands</a>
-				with the lower arms removed.
+				My external monitor is a <a href="https://www.sceptre.com/Monitors/2K-4K-Series/C355W-3440UN-35-Curved-Monitor-product1176category12category98.html" target="_blank">Scepter C355W-3440UN 35" Curved Monitor</a>.
 			</li>
 			<li>
-				He told me one of the best investments he ever made was splurging on a <a href="https://www.zsa.io/moonlander" target="_blank">Moonlander Mark I keyboard</a>. It's a bit
-				pricey, but has easily been the most comfortable and customizable keyboard he's ever used.
+				One of the best investments I ever made was splurging on a <a href="https://www.zsa.io/moonlander" target="_blank">Moonlander Mark I keyboard</a>. It's a bit
+				pricey, but has easily been the most comfortable and customizable keyboard I've ever used.
 			</li>
 			<li>
-				His mouse is a <a href="https://amzn.to/3yYjwrj" target="_blank">ZLOT Vertical Gaming Mouse</a>.
-				It's lasted for years and he really likes the ergonomics of it.
+				My mouse is a <a href="https://amzn.to/3yYjwrj" target="_blank">ZLOT Vertical Gaming Mouse</a>.
+				It's lasted for years and I really like the ergonomics of it.
 			</li>
 			<li>
-				He has been using the same <a href="https://amzn.to/3Msi3MT" target="_blank">Blue Yeti microphone</a> for probably 10 years now and
-				it's still going strong. He doesn't do a lot of recording or anything, but when he does it's always been great.
+				I've been using the same <a href="https://amzn.to/3Msi3MT" target="_blank">Blue Yeti microphone</a> for probably 10 years now and
+				it's still going strong.
 			</li>
 			<li>
-				His webcam is a generic <a href="https://amzn.to/3XCfdvr" target="_blank">1080P Webcam with Microphone</a>
-				that was a gift several years ago and it's been working great. He will probably upgrade to something better soon, but for now it's been perfect.
+				I use a <a href="https://amzn.to/3UFtsgU" target="_blank">Logitech for Creators StreamCam Premium Webcam</a> for video calls and recording videos.
 			</li>
 			<li>
-				With all these monitors and devices, he needed something to keep them all connected without having to deal with a bunch of cables...not to mention there are only
-				so many ports on a Macbook. So he picked up a
+				To light my office, I use 2 <a href="https://amzn.to/3ABCRPL" target="_blank">Neewer LED Panel Lights</a> that I bounce off the walls.
+			</li>
+			<li>
+				With all these devices, I needed something to keep them all connected without having to deal with a bunch of cables...not to mention there are only
+				so many ports on a Macbook. So I picked up a
 				<a href="https://amzn.to/4cRDcen" target="_blank">StarTech.com Thunderbolt 3 to Dual DisplayPort Adapter</a>,
 				a <a href="https://amzn.to/3XauWjH" target="_blank">Selore&S-Global USB C to Dual HDMI Adapter</a>, and a
 				<a href="https://amzn.to/4g1zBgF" target="_blank">13 port LATORICE Powered USB Hub</a>
 				to plug them all into. Then those are all connected via an
 				<a href="https://amzn.to/4cQfrTK" target="_blank">OWC 5 Port Thunderbolt Hub</a>.
-				Now when he gets to his desk, all he has to do is plug in 1 USB C cable and everything is good to go!
+				Now when I get to my desk, all I have to do is plug in 1 USB C cable and everything is good to go!
 			</li>
 			<li>
-				Jake always hated having to carry around all kinds of bricks and adapters to plug in and charge stuff. He searched for a while
-				and nothing really worked great, but finally he found this
+				I've always hated having to carry around all kinds of bricks and adapters to plug in and charge stuff. I searched for a while
+				and nothing really worked great, but finally I found this
 				<a href="https://amzn.to/3Zaw8WS" target="_blank">Portable 7 Port Fast Charging Station</a>.
-				Now he can charge his laptop, phone, iPad, and anything else he needs all at the same time.
+				Now I can charge my laptop, phone, iPad, and anything else I need all at the same time.
 			</li>
 			<li>
-				He also tried a bunch of different USB C cables, but his favorite one by far has to be the 
-				<a href="https://amzn.to/4cK6P1b" target="_blank">Dbilida 6.6ft Thunderbolt 3 Cable (40Gbps)</a>. He's
-				had it for a long time and it's still going strong.
+				I also tried a bunch of different USB C cables, but my favorite one by far has to be the
+				<a href="https://amzn.to/4cK6P1b" target="_blank">Dbilida 6.6ft Thunderbolt 3 Cable (40Gbps)</a>. I've
+				had it for a long time and it's still doing great!
 			</li>
 			<li>
-				He carries everything in a <a href="https://amzn.to/3Xc3JwY" target="_blank">REEBOW GEAR Military Tactical Backpack Large Army 3 Day
+				I carry everything in a <a href="https://amzn.to/3Xc3JwY" target="_blank">REEBOW GEAR Military Tactical Backpack Large Army 3 Day
 				Assault Pack Molle Bag Backpack</a> The Molle system is fantastic when you have lots of gadgets and things you need to keep organized.
 			</li>
 			<li>
-				He listens to <a href="https://amzn.to/3yYjMXj" target="_blank">Beats Fit Pro</a> earbuds most of the time, whether he's at his desk or
+				I listen to <a href="https://amzn.to/3yYjMXj" target="_blank">Beats Fit Pro</a> earbuds most of the time, whether I'm at my desk or
 				out and about.
 			</li>
 			<li>
-				But when he doesn't have his headerphones in, he has a <a href="https://amzn.to/3AGTlWs" target="_blank">Creative Pebble Plus 2.1
-				USB-Powered Desktop Speakers with Powerful Down-Firing Subwoofer</a> plugged in to his laptop for when he wants to jam out.
+				But when I don't have my earbuds in, I have a <a href="https://amzn.to/3AGTlWs" target="_blank">Creative Pebble Plus 2.1
+				USB-Powered Desktop Speakers with Powerful Down-Firing Subwoofer</a> plugged in to my laptop for when I want to jam out.
 			</li>
 			<li>
 				Another small but awesome little piece of gear is the <a href="https://amzn.to/3Tel2wb" target="_blank">Luxtude electronic organizer case</a>.
-				This thing is fantastic for storing and organizing all his cables and accessories.
+				This thing is fantastic for storing and organizing all my cables and accessories.
 			</li>
 			<li>
-				After a long search for the right power bank to carry with him, he finally found the
+				After a long search for the right power bank to carry with me, I finally found the
 				<a href="https://amzn.to/3AIFCym" target="_blank">Omni 20+ 20000mah Laptop Power Bank Portable Charger</a>. It's been the
-				perfect companion for his laptop and phone when he's on the go.
+				perfect companion for my laptop and phone when I'm on the go.
 			</li>
 		</ul>
 	</Section>
@@ -113,44 +106,45 @@
 
 		<ul>
 			<li>
-				Most of his development is done in <a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a> with the
+				Recently I start testing out <a href="https://www.cursor.com/" target="_blank">Cursor</a> as my primary editor with the
 				<a href="https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme" target="_blank">GitHub Dark Default</a> theme.
-				But he was a longtime Vim user, so he still opens that up from time to time to edit files and make quick changes. He has a custom theme called
-				<a href="https://github.com/iamthe-Wraith/breeze" target="_blank">Breeze</a> he uses for Vim that's a fork of the
+				So far the experience has been great, but once GitHub Copilot and VSCode start offering similar features, I will likely switch back.
+				<br>
+				<br>
+				But I was a longtime Vim user too, so I still open that up from time to time to edit files and make quick changes. I have a custom theme called
+				<a href="https://github.com/iamthe-Wraith/breeze" target="_blank">Breeze</a> I use for Vim that's a fork of the
 				<a href="https://github.com/dracula/vim" target="_blank">Dracula</a> theme.
 			</li>
 			<li>
-				A couple years ago Jake switched to using the <a href="https://www.warp.dev/" target="_blank">Warp</a> terminal and has never looked back.
-				It's a fantastic tool with so many little benefits he could (and does) go on about it for a while...but don't worry, I won't do that to you.
+				Not too long ago I switched to using the <a href="https://www.warp.dev/" target="_blank">Warp</a> terminal and have never looked back.
+				It's a fantastic tool with so many little benefits I could (and do) go on about it for a while...but don't worry, I won't do that to you.
 			</li>
 			<li>
-				<a href="https://arc.net/" target="_blank">Arc</a> is his primary browser of choice, which he uses for just about everything.
+				<a href="https://arc.net/" target="_blank">Arc</a> is my primary browser of choice, which I use for just about everything.
 			</li>
 			<li>
-				He replaced Mac's built in Spotlight with <a href="https://www.raycast.com/" target="_blank">Raycast</a> (Pro version) for quick and easy access to
+				I replaced Mac's built in Spotlight with <a href="https://www.raycast.com/" target="_blank">Raycast</a> (Pro version) for quick and easy access to
 				multiple AI models and a bunch of plugins.
 			</li>
 			<li>
-				For note taking he uses <a href="https://obsidian.md/" target="_blank">Obsidian</a> with a few different vaults to help keep things organized. Particularly
-				his "slip-box", which is where he keeps all his notes and ideas...a method he learned from one of his favorite books,
+				For note taking I use <a href="https://obsidian.md/" target="_blank">Obsidian</a> with a few different vaults to help keep things organized. Particularly
+				my "second brain", which is where I keep all my notes and ideas...a method I learned from one of my favorite books,
 				<a href="https://amzn.to/3ZbBhOr" target="_blank">How to Take Smart Notes</a>.
 			</li>
 			<li>
-				<a href="https://1password.com/" target="_blank">1Password</a> is his password manager of choice. It used to be LastPass, but he switched a few years ago
-				after some security concerns.
+				I've been using <a href="https://1password.com/" target="_blank">1Password</a> as my password manager for years now. It used to be LastPass, but I switched
+				a few years ago after some security concerns.
 			</li>
 			<li>
-				He's been a loyal <a href="https://proton.me/mail" target="_blank">Proton</a> customer for many years now. He uses it for all his personal email,
+				I've been a loyal <a href="https://proton.me/mail" target="_blank">Proton</a> customer for many years now. I use it for all my personal email,
 				encrypted cloud storage, and VPN.
 			</li>
 			<li>
-				He built <a href="https://buzybee.buzz" target="_blank">BuzyBee</a> to help himself manage all his tasks and projects using the
-				<a href="https://gettingthingsdone.com/" target="_blank">GTD Methodology</a>, and uses it every day. It's not currently
-				available to the public, but be sure to sign up for the waitlist if you're interested and we will let you know as soon as it
-				becomes available!
+				I built <a href="https://buzybee.buzz" target="_blank">BuzyBee</a> to help myself manage all my tasks and projects using the
+				<a href="https://gettingthingsdone.com/" target="_blank">GTD Methodology</a>, and use it every day.
 			</li>
 			<li>
-				He recently switched to <a href="https://omnivore.app" target="_blank">Omnivore</a> to manage, organize and track all his reading. He was using
+				I also recently switched to <a href="https://omnivore.app" target="_blank">Omnivore</a> to manage, organize and track all my reading. I was using
 				<a href="https://readwise.io/" target="_blank">Readwise</a>, but wasn't really happy with it. Omnivore has been a great replacement.
 			</li>
 		</ul>
